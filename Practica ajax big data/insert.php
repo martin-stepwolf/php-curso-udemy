8255eb07a74547d32f53d20e5b0c90d89bb3288c
IngCampos <15151130@mail.ita.mx>
2019-04-04 14:24:11 -0600

    Adaptacion mysqli_connect
    Esta forma permite extraer un mensaje de error en caso de que no se ejecute la consulta.
    La forma de PDO es desconocido la posisbilidad de hacer lo mismo

--- Practica ajax big data/insert.php
+++ Practica ajax big data/insert.php
@@ -8,6 +8,16 @@
       $conexion2 = new PDO('mysql:host=localhost; dbname=ajax;','root','');
       $statement2 = $conexion2->prepare("INSERT INTO user (name, email, password, gender) VALUES ('$name', '$email', '$password', '$gender')");
       $statement2->execute();
+      
+      //Create connection
+  $connection = mysqli_connect('localhost', 'root', '', 'ajax');
+  $q = "INSERT INTO user (name, email, password, gender) VALUES ('$name', '$email', '$password', '$gender')";
+  $query = mysqli_query($connection, $q);
+  if($query){
+      echo json_encode("Data Inserted Successfully");
+      }
+  else {
+      echo json_encode('problem');
       }
 
 ?>
