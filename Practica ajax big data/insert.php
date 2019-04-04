346a339cb37efe2ae587f80a286c2ae5e6c838bf
IngCampos <15151130@mail.ita.mx>
2019-04-04 14:25:19 -0600

    Apartado de codigo anterior

--- Practica ajax big data/insert.php
+++ Practica ajax big data/insert.php
@@ -4,12 +4,12 @@
       $email = $_POST['email'];
       $password= $_POST['password'];
       $gender = $_POST['gender'];
-
+      /* forma actual que se usa
       $conexion2 = new PDO('mysql:host=localhost; dbname=ajax;','root','');
       $statement2 = $conexion2->prepare("INSERT INTO user (name, email, password, gender) VALUES ('$name', '$email', '$password', '$gender')");
-      $statement2->execute();
+      $statement2->execute();*/
       
-      //Create connection
+      //Create connection, forma antigua
   $connection = mysqli_connect('localhost', 'root', '', 'ajax');
   $q = "INSERT INTO user (name, email, password, gender) VALUES ('$name', '$email', '$password', '$gender')";
   $query = mysqli_query($connection, $q);
